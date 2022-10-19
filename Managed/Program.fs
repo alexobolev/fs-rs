65c16cadf53a9697df365ae99aed0ba9a6e2f25f
Alex Sobolev <code.sobolev@gmail.com>
2022-10-19 05:18:38 +0300

    Add native interface + managed code.

---
+++ Managed/Program.fs
@@ -0,0 +1,60 @@
+﻿open System
+open System.Runtime.InteropServices
+
+module internal InteropInternal =
+
+    [<DllImport("Native", CallingConvention = CallingConvention.Cdecl)>]
+    extern IntPtr internal BuilderCreate (string name, int32 number)
+    
+    [<DllImport("Native", CallingConvention = CallingConvention.Cdecl)>]
+    extern void internal BuilderFree (IntPtr pointer)
+    
+    [<DllImport("Native", CallingConvention = CallingConvention.Cdecl)>]
+    extern int32 internal BuilderGetNumber (IntPtr pointer)
+
+    [<DllImport("Native", CallingConvention = CallingConvention.Cdecl)>]
+    extern void internal BuilderSetNumber (IntPtr pointer, int32 value)
+
+module public Interop =
+
+    type Builder(name: string, number: int) =
+
+        // call rust constructor or managed initialization
+        let handle : IntPtr = InteropInternal.BuilderCreate (name, number)
+
+        // field to store destruction status
+        let mutable disposed : bool = false
+
+        let cleanup (disposing : bool) =
+            if not disposed then
+                disposed <- true
+                // if disposing then
+                //     ()
+                InteropInternal.BuilderFree (handle)
+
+        interface IDisposable with
+            member self.Dispose() =
+                cleanup (true)
+                GC.SuppressFinalize(self)
+
+        override self.Finalize() =
+            cleanup (false)
+
+        member self.Number
+            with get () : int32 = InteropInternal.BuilderGetNumber (handle)
+            and set (value : int32) = InteropInternal.BuilderSetNumber (handle, value)
+
+    
+
+[<EntryPoint>]
+let main args =
+
+    printfn "Hello from F#!"
+
+    for index, arg in Seq.indexed args do
+        printfn "Arg #%d => %s." index arg
+
+    use foo = new Interop.Builder ("John", 15)
+    printfn "Builder was initialized with value %d" foo.Number
+
+    0
--- Native/src/interface.rs
+++ Native/src/interface.rs
@@ -4,7 +4,7 @@ use crate::implementation::{Builder};
 
 
 #[no_mangle]
-pub unsafe extern "C" fn nat_builder_create<'a>(name: *const c_char, number: c_int)
+pub unsafe extern "C" fn BuilderCreate<'a>(name: *const c_char, number: c_int)
     -> *mut Builder<'a>
 {
     println!("[INTERF] Initializing a Builder instance...");
@@ -17,10 +17,28 @@ pub unsafe extern "C" fn nat_builder_create<'a>(name: *const c_char, number: c_i
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn nat_builder_free<'a>(this: *mut Builder<'a>)
+pub unsafe extern "C" fn BuilderFree<'a>(this: *mut Builder<'a>)
     -> ()
 {
     println!("[INTERF] Dropping a Builder instance at {:p}...", this);
     let boxed = Box::from_raw(this);
     drop(boxed);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn BuilderGetNumber<'a>(this: *mut Builder<'a>)
+    -> c_int
+{
+    let builder = this.as_ref()
+        .expect("failed to get builder reference");
+    builder.get_number()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn BuilderSetNumber<'a>(this: *mut Builder<'a>, value: c_int)
+    -> ()
+{
+    let builder = this.as_mut()
+        .expect("failed to get builder mut reference");
+    builder.set_number(value as i32);
 }
